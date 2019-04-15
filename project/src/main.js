2314a4d83ee09cd13b330626efdd3838738ac064
shenzhuang <shenzhuang@yonyou.com>
2019-04-15 15:43:51 +0800

    feature: refactor

--- project/src/main.js
+++ project/src/main.js
@@ -2,10 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
+import lodash from 'lodash'
 
+// css
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/reset.css'
+import 'vue2-animate/dist/vue2-animate.min.css'
+
+// 全局挂载lodash
+Vue.prototype.lodash = lodash
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
