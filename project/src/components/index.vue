bd76c9ef224b1b6f6a4e7352e314ebe72bf39ade
windrunner <smilehero@outlook.com>
2019-04-15 17:01:53 +0800

    feature: add the waterfall component

--- project/src/components/index.vue
+++ project/src/components/index.vue
@@ -20,66 +20,15 @@
       </div>
     </div>
     <!-- 瀑布流开始 -->
-    <div class="waterfall-starts">
-      <div class="column">
-        <div class="item">
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-          <div class="item--middle"></div>
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="item">
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--middle"></div>
-          <div class="item--large"></div>
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-          <div class="item--small"></div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="item">
-          <div class="item--middle"></div>
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--middle"></div>
-          <div class="item--large"></div>
-          <div class="item--middle"></div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="item">
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-          <div class="item--middle"></div>
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="item">
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--middle"></div>
-          <div class="item--small"></div>
-          <div class="item--large"></div>
-          <div class="item--small"></div>
-          <div class="item--small"></div>
-        </div>
-      </div>
-    </div>
+    <water-fall></water-fall>
   </div>
 </template>
 
 <script>
+  import waterFall from './childComponents/waterFall'
   export default {
     name: "index.vue",
+    components: { waterFall },
     data() {
       return {
         main_title: '正是江南好风景,落花时节又逢君'
@@ -116,43 +65,6 @@
             cursor: pointer;
           }
         }
-      }
-    }
-    .waterfall-starts {
-      display: flex;
-      flex-flow: column wrap;
-    }
-
-    @media only screen and (min-width: 500px) {
-      .waterfall-starts {
-        flex-direction: row;
-      }
-    }
-    .column {
-      display: flex;
-      flex-flow: column wrap;
-      width: 100%;
-      .item {
-        box-sizing: border-box;
-        padding: 10px;
-        counter-increment: item-counter;
-        &--large {
-          min-height: 260px;
-          background: aliceblue;
-        }
-        &--middle {
-          min-height: 180px;
-          background: beige;
-        }
-        &--small {
-          min-height: 100px;
-          background: palevioletred;
-        }
-      }
-    }
-    @media only screen and (min-width: 500px) {
-      .column {
-        width: calc(100% / 5);
       }
     }
   }
